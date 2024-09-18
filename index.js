f9b7aa0d9370cdcd1ec837f11ba14f5eaa3b1852
Tharaniesh <99646275+Tharaniesh3@users.noreply.github.com>
2024-09-18 19:59:27 +0530

    Commited for seo

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const express = require("express");
-const app = express();
+const app = require("express")();
 require("dotenv").config();
 const port = process.env.PORT || 4001;
 const router = require("./router/index.js");
 const cors = require("cors");
 
-// Enable parsing JSON
 app.use(express.json());
 
-// CORS configuration
 const corsOptions = {
-  origin: ['https://cure-centralized-user-registry-for-experts-m6vp.vercel.app', 'http://localhost:5173'],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", // Add OPTIONS method
-  credentials: true, // Enable credentials
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add any other headers you need
+  origin: ["http://localhost:5173","https://cure-centralized-user-registry-for-experts-m6vp.vercel.app","https://cure-centralized-user-registry-for-experts-backend.vercel.app"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
 };
 
-// Apply CORS to all routes
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); 
 
-// Handle preflight requests explicitly for all routes
-app.options('*', cors(corsOptions));
-
-// Define the routes
 app.use("/api", router);
 
-// A test route to verify CORS functionality
-app.get("/test-cors", (req, res) => {
-  res.json({ message: "CORS test successful" });
-});
-
-// Start the server
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
